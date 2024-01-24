9da0f4ad3ec52f3304b532bfc33f589012fc7742
aniketshelke8010 <aniketshelke9900@gmail.com>
2024-01-25 01:05:55 +0530

    car

--- style/js/index.js
+++ style/js/index.js
@@ -6,11 +6,11 @@ var travel = document.getElementById("travel");
 
 //Banners
 
-var carBanner = document.querySelector("a_car_service");
-var bikeBanner = document.querySelector("a_bike_service");
-var healthBanner = document.querySelector("a_health_service");
-var lifeBanner = document.querySelector("a_life_service");
-var travelBanner = document.querySelector("a_travel_service");
+var carBanner = document.querySelector(".a_car_service");
+var bikeBanner = document.querySelector(".a_bike_service");
+var healthBanner = document.querySelector(".a_health_service");
+var lifeBanner = document.querySelector(".a_life_service");
+var travelBanner = document.querySelector(".a_travel_service");
 
 window.onload = function () {
   car.classList.add("active");
@@ -63,5 +63,37 @@ health.addEventListener("click", function (event) {
   healthBanner.classList.remove("d-none");
   lifeBanner.classList.add("d-none");
   travelBanner.classList.add("d-none");
+  carBanner.classList.add("d-none");
+});
+
+life.addEventListener("click", function (event) {
+  bike.classList.remove("active");
+  life.classList.add("active");
+  health.classList.remove("active");
+  travel.classList.remove("active");
+  car.classList.remove("active");
+
+  //banner
+
+  bikeBanner.classList.add("d-none");
+  healthBanner.classList.add("d-none");
+  lifeBanner.classList.remove("d-none");
+  travelBanner.classList.add("d-none");
+  carBanner.classList.add("d-none");
+});
+
+travel.addEventListener("click", function (event) {
+  bike.classList.remove("active");
+  health.classList.remove("active");
+  life.classList.remove("active");
+  travel.classList.add("active");
+  car.classList.remove("active");
+
+  //banner
+
+  bikeBanner.classList.add("d-none");
+  healthBanner.classList.add("d-none");
+  lifeBanner.classList.add("d-none");
+  travelBanner.classList.remove("d-none");
   carBanner.classList.add("d-none");
 });
